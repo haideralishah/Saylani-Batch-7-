71df5f15bb630adb081f791dec3dea3453d6eb85
Mohammad <mohammadsaadali786.92@gmail.com>
2021-03-31 11:04:01 +0500

    Chapter 1 to 9 completed

---
+++ JS/Chapter 1 - 9/app.js
@@ -0,0 +1,314 @@
+// string
+// number
+// boolean
+
+// 03413542800
+// 1500
+
+
+// alert("Hello World");
+// alert("This is JavaScript World");
+
+// alert(2);
+
+
+// console.log("Hello to Console World");
+// console.log("5 + 5");
+// console.log(5 + 5);
+
+// Welcome to "Saylani Welfare International Trust".
+
+// console.log('Welcome to "Saylani Welfare International Trust".');
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName1, userName2, userName3, userName4, userName5;
+
+// userName = "Waseem";
+// userName3 = 'Akram';
+
+// var userName = "Waseem";
+// console.log(userName1);
+// var userName = prompt('What is your name?'); //"haider"
+// console.log(userName)
+
+
+
+
+// var age = +prompt('What is your age?', 5);
+// console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var isPakistani = confirm("Are you Pakistani?");
+// console.log(isPakistani);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*******************/
+/******Numbers******/
+/*******************/
+
+// + - * / % **
+
+
+
+// ++ -- 
+
+
+// var num1 = num2 - num1;
+// var difference3 ;
+// var num2 = 7;
+// var sum = num1 + num2;
+
+// var difference1 = num1 - num2;
+// var difference2 = = num2 - num2;
+
+// console.log(difference1); // -2
+// console.log(difference2); // 2
+// console.log(difference3); // 0 
+
+// var multiplication = num1 * num2;
+// console.log(multiplication)
+
+
+
+
+
+
+
+
+
+// var num3 = 8;
+// var num4 = 10;
+// var divid = num4 / num1; // 10 / 5
+// console.log(divid);
+
+
+
+
+
+
+// var num5 = 100;
+// var num6 = 9;
+
+// var mod = num5 % num6;
+// var mod = num5 % num2;
+
+// console.log(mod);
+
+
+
+
+// var exponent = num1**2;
+// var exponent = num1**3;
+// console.log(exponent)
+
+
+
+
+
+
+// var userName = "haider";
+// console.log(userName);
+
+// userName = 'Ali';
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num10 = 5; // 5
+// var num11 = num10++; // 5 
+// var num12 = num10++; // 6
+// var num13 = num11--; // 5
+// var num14 = --num11; // 3
+
+// console.log(num10, num11, num12, num13, num14); // 7 3 6 5 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 3;
+//5     +    4    +  6    -  4     -   6
+// console.log(num1++ + ++num2 + num1++ - num2-- - --num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 2;
+// var num2 = 4;
+// var num3 = '6';
+// console.log(num1 + num2);
+// var firstName = 'haider';
+// var lastName = 'ali';
+// console.log(num2 + num1 + num3);
+// console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var userName = "haider";
+
+
+if(true){
+    console.log('He is JS trainer.');
+    console.log('He is JS trainer.');
+    console.log('He is JS trainer.');
+    console.log('He is JS trainer.');
+    console.log('He is JS trainer.');
+    console.log('He is JS trainer.');
+}
+
+
+
+
+
+
+
