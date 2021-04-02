36cb0477d677cc0ac3cc048777c3c37bef0e457d
Mohammad <mohammadsaadali786.92@gmail.com>
2021-04-02 11:08:15 +0500

    class code till if statement. assignment chapter 1 to 9

---
+++ JS/Chapter 10/app.js
@@ -0,0 +1,98 @@
+
+/*
+= assignment operator
+
+=== equal operator
+
+!==
+
+<
+>
+
+
+
+*/
+
+// var num1 = 5;
+
+// if (num1 <= 10) {
+//     console.log('buy aloo piyaz');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = +prompt('what is your age?');
+// var requiredAge = 14;
+
+// if (age > requiredAge) {
+//     console.log('Welcome to Cricket Club');
+// }
+// else {
+//     console.log('Please try again once you are older than ' + requiredAge + ' years.')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+var currentMonth = prompt('what is the name of current month?');
+var monthOfBlessing = "Ramadan";
+
+if (currentMonth === monthOfBlessing) {
+    console.log('Welcome to the month of blessings.');
+}
+else {
+    console.log('Wait till ' + monthOfBlessing);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS/JS assignment 1 - 7/app.js
@@ -0,0 +1,150 @@
+/*
+alert();
+console.log();
+prompt();
+confirm();
+var
+mathematical operations
+++ --
+concatination
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/**********PDF 2**********/
+
+/***********Q:1***********/
+var username;
+
+
+/***********Q:2***********/
+var myName = 'Haider Ali Shah';
+
+/***********Q:3***********/
+var message;
+message = 'Hello World';
+// alert(message);
+
+
+/***********Q:4***********/
+// var studentName = 'Akram Khan';
+// var age = '17';
+// var institute = 'Saylani Mass IT Training';
+// var courseName = 'Web, Mobile and Cloud Computing';
+
+// console.log(studentName);
+// console.log(age + ' years old.');
+// console.log(institute);
+// console.log(courseName);
+
+
+
+
+
+
+
+
+
+
+
+// /***********Q:5***********/
+// var food = 'Pizza\nPizz\npiz\npi\np';
+// alert(food);
+
+
+
+// /***********Q:6***********/
+// var partnerName = 'abc';
+// var kids = 2;
+// var designation = 'Software Engineer'
+// var countryName = 'USA';
+
+// console.log('You will be a ' + designation + ' in ' + countryName + ', and married to ' + partnerName + ' with ' + kids + ' kids.');
+
+
+
+
+
+// /***********Q:10***********/
+// document.write("<h1>Hello World</h1>");
+
+// document.write("<p>Yay! I can write HTML content through JavaScript</p>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********PDF 3**********/
+
+/***********Q:1***********/
+
+// var num1 = +prompt('Please tell a number.');
+// var num2 = +prompt('Please tell another number.');
+// var sum = num1 + num2;
+// console.log("sum of " + num1 + ' and ' + num2 + ' is ' + sum);
+
+
+
+
+/***********Q:2***********/
+
+
+
+// var num1 = +prompt('Please tell a number.');
+// var num2 = +prompt('Please tell another number.');
+// var subtraction = num1 - num2;
+// console.log("subtraction of " + num1 + ' and ' + num2 + ' is ' + subtraction);
+
+
+
+
+// var num1 = +prompt('Please tell a number.');
+// var num2 = +prompt('Please tell another number.');
+// var product = num1 * num2;
+// console.log("Product of " + num1 + ' and ' + num2 + ' is ' + product);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***********Q:5***********/
+var table = +prompt('Tell us a table name');
+var times = 1
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
+console.log(table + ' X ' + times + ' = ' + table * times++);
