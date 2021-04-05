a50772313f65811ebe088ef0e59af1fa17506fc5
haideralishah <headeralishah@gmail.com>
2021-04-05 11:01:53 +0500

    if statement and array introduction

---
+++ JS/Chapter 10 - 13/app.js
@@ -0,0 +1,265 @@
+
+/*
+= assignment operator
+
+=== equal operator
+
+!==
+
+<
+>
+
+
+
+*/
+
+// var num1 = 5;
+
+// if (num1 <= 10) {
+//     console.log('buy aloo piyaz');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = +prompt('what is your age?');
+// var requiredAge = 14;
+
+// if (age > requiredAge) {
+//     console.log('Welcome to Cricket Club');
+// }
+// else {
+//     console.log('Please try again once you are older than ' + requiredAge + ' years.')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var currentMonth = prompt('what is the name of current month?');
+// var monthOfBlessing = "Ramadan";
+// var monthOfBlessing1 = "ramadan";
+
+// if (currentMonth === monthOfBlessing) {
+//     console.log('Welcome to the month of blessings.');
+// }
+// else if (currentMonth === monthOfBlessing1) {
+//     console.log('Welcome to the month of blessings.');
+// }
+// else {
+//     console.log('Wait till ' + monthOfBlessing);
+// }
+
+
+
+
+
+
+// var num = +prompt('tell a number');
+
+// if (num > 20) {
+//     console.log('number is greater than 20');
+// }
+// else if (num > 15) {
+//     console.log('number is greater than 15');
+// }
+// else if (num > 10) {
+//     console.log('number is greater than 10');
+// }
+// else if (num > 5) {
+//     console.log('number is greater than 5');
+// }
+// else {
+//     console.log('number is less than 5');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// &&  And
+// ||  Or
+// var shopName = 'Mudassir';
+// var priceLimit = 100;
+
+// if ("Mudassir" === shopName && 200 < priceLimit) {
+//     console.log('Bring 1 KG daal');
+// }
+// else{
+//     console.log('Bring coocked daal');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var snacks = 'Lays';
+// var snacks1 = 'kurkure';
+// var coldDrink = 'Pepsi';
+
+// if(snacks === 'Lays' && coldDrink === 'Dew'){
+//     console.log("Bring Lays and Pepsi");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var percentage = +prompt('what is your matric percentage?');
+
+// if (percentage >= 80 && percentage <= 100) {
+//     console.log('You have got A1');
+// }
+// else if (percentage >= 70 && percentage < 80) {
+//     console.log('You have got A');
+// }
+// else{
+//     console.log('something went wrong.');
+// }
+
+
+
+
+
+
+
+
+// var num1 = +prompt("tell a number");
+// var num2 = +prompt("tell another number");
+
+// if (num1 > num2) {
+//     console.log(num1);
+// }
+// else if (num2 > num1) {
+//     console.log(num2)
+// }
+// else {
+//     console.log('something went wrong.');
+// }
+
+
+
+
+
+
+
+
+var num1 = +prompt("tell a number");
+
+if (num1 > 0) {
+    console.log('user input is positive integer.');
+}
+else if (num1 < 0) {
+    console.log('user input is negative integer.');
+}
+else if (num1 === 0) {
+    console.log('user input is 0.');
+}
+else{
+    console.log('something went wrong');
+}
---
+++ JS/Chapter 15/app.js
@@ -0,0 +1,13 @@
+// var city1 = "karachi";
+// var city2 = "lahore";
+// var city3 = "peshawar";
+var cars = [];
+
+// var cities = ["karachi", "lahore", 'peshawar'];
+
+// console.log(cities[2]);
+
+cars[0] = "Karachi";
+cars[2] = "Peshawar";
+console.log(cars);
+
