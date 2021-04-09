eee1499f50d3ad9e7aea1e64712e49098b7689e8
haideralishah <headeralishah@gmail.com>
2021-04-09 10:46:28 +0500

    class code till array

---
+++ JS/07-04-2021/app.js
@@ -0,0 +1,77 @@
+// if (c === d) {
+//     if (x === y) {
+//         g = h;
+//         }
+//     else if (a === b) {
+//         g = h;
+//         }
+//     else {
+//         e = f;
+//         }
+// }
+// else {
+//      e = f;
+//      }
+    
+// var names ="abdullah";
+// names ="arsalan"
+
+// var cities = ["Atlanta","Paris","Denver","Karachi",3,4,true,null];
+
+// console.log(cities,'cityyyyyy');
+
+
+// console.log(cities[2])
+
+// var pets = [];
+
+// pets[0] = "cat";
+// pets[1] = 'lion';
+
+// pets[4] ='sparrow';
+// console.log(pets,'petspetspets')
+// console.log(pets[3]);
+
+var pets = [];
+
+// pets[0] = "cat";
+// pets[1] = 'lion';
+// pets[2] = 'pigeon';
+// pets[3] = 'tiger';
+// pets[4] ='sparrow';
+
+// console.log(pets);
+// var getValue =pets.pop()
+// console.log(getValue,'getValuegetValuegetValue')
+// console.log(pets.pop())
+// console.log(pets);
+
+
+// var pets = [];
+// pets[0] = "cat";
+// pets[1] = 'lion';
+// pets[2] = 'pigeon';
+// pets[3] = 'tiger';
+// pets[4] ='sparrow';
+
+// console.log(pets);
+
+// pets.push("snake",3,null)
+// console.log(pets);
+
+
+
+var pets = [];
+pets[0] = "cat";
+pets[1] = 'lion';
+pets[2] = 'pigeon';
+pets[3] = 'tiger';
+pets[4] ='sparrow';
+
+console.log(pets);
+
+pets.shift()
+console.log(pets);
+
+pets.unshift('elephant')
+console.log(pets);
--- JS/Chapter 15/app.js
+++ JS/Chapter 15/app.js
@@ -1,13 +1,230 @@
 // var city1 = "karachi";
 // var city2 = "lahore";
 // var city3 = "peshawar";
-var cars = [];
+// var cars = [];
 
 // var cities = ["karachi", "lahore", 'peshawar'];
 
-// console.log(cities[2]);
+// // console.log(cities[2]);
 
-cars[0] = "Karachi";
-cars[2] = "Peshawar";
-console.log(cars);
+// cars[0] = "Karachi";
+// cars[2] = "Peshawar";
+// console.log(cars);
+
+
+
+
+
+
+// var something = ['haider', 2643, true, ['apple', 'banana'], '021-36912345'];
+// console.log(something[3][1]);
+
+
+// var students = ['haider', 'akram', 'akbar'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+properties
+method
+
+
+.length
+
+.push()
+.unshift()
+
+
+.pop()
+.shift()
+
+.splice()
+.slice()
+
+
+*/
+
+
+// var cities = ['Karachi', 'Lahore', 'Islamabad'];
+// // console.log(cities.length);
+// console.log(cities);
+// cities.push('Peshawar');
+// console.log(cities);
+// cities.unshift('Quetta');
+// console.log(cities);
+// cities.pop();
+// cities.shift();
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = +prompt('tell a number');
+// var operator = prompt('tell an operator');
+// var num2 = +prompt('tell another number');
+
+// if (operator === '+') {
+//     console.log(num1 + num2);
+// }
+// else if (operator === '-') {
+//     console.log(num1 - num2);
+// }
+// else if (operator === '*') {
+//     console.log(num1 * num2);
+// }
+// else if (operator === '/') {
+//     console.log(num1 / num2);
+// }
+// else{
+//     console.log('Something went wrong.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cities = [prompt('tell a city name'), prompt('tell a city name'), prompt('tell a city name'), prompt('tell a city name'), prompt('tell a city name')];
+
+
+// console.log(cities);
+
+
+
+
+
+
+
+// var city1 = prompt('tell a city name');
+// var city2 = prompt('tell a city name');
+// var city3 = prompt('tell a city name');
+// var city4 = prompt('tell a city name');
+// var city5 = prompt('tell a city name');
+
+// var cities = [city1, city2, city3, city4, city5];
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'cherry', 'orange'];
+
+// // fruits.splice(1, 2);
+// fruits.splice(2, 1, "Mango", "Pineapple");
+
+// console.log(fruits);
+
+// var mangoIndex = fruits.indexOf("Mango");
+// console.log(mangoIndex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'cherry', 'orange'];
+// console.log(fruits);
+
+// var dislikedFruit = prompt('let us know least liked fruit.');
+// var likedFruit = prompt('let us know favourite fruit.');
+
+// var dislikedIndex = fruits.indexOf(dislikedFruit);
+// fruits.splice(dislikedIndex, 1, likedFruit);
+// console.log(fruits);
+
+
+
+
+
+
+
+
 
