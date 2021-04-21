8bdbbad4d9407ec41b2269e28263757e17fe4f28
haideralishah <headeralishah@gmail.com>
2021-04-21 10:04:42 +0500

    numbers methods.

---
+++ JS/chapter 26/app.js
@@ -0,0 +1,152 @@
+// var num1 = 3.5
+
+// console.log(num1);
+
+// num1 = Math.round(num1);
+
+// console.log(num1);
+
+
+
+
+
+
+
+/*
+
+Math.ceil()
+Math.floor()
+
+*/
+
+
+// var num1 = 3.1
+// var num2 = 3.9
+
+// // num1 = Math.ceil(num1)
+// num2 = Math.floor(num2);
+
+// console.log(num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = -0.9;
+
+// num1 = Math.ceil(num1);
+// console.log(num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = -0.1;
+
+// num1 = Math.floor(num1);
+// console.log(num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// heads 
+// tails
+
+// var randomNumber = Math.floor(Math.random() * 51);
+
+// console.log(randomNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var userPick = prompt('What do you pick?');
+
+var coinResult = Math.ceil(Math.random() * 2);
+
+if (coinResult === 1) {
+    console.log('Coin is heads.');
+}
+else if (coinResult === 2) {
+    console.log('Coin is tails.');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
