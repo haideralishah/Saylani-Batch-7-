5f01d0455c8dd29abb0a80feca630a761089c869
haideralishah <headeralishah@gmail.com>
2021-04-26 09:56:43 +0500

    class code till Date String.

---
+++ JS/Chapter 31/app.js
@@ -0,0 +1,177 @@
+/*
+alert();
+console.log();
+prompt();
+confirm();
+
+Number
+Arthemetic Operation
++-* /% ++ --
+
+String 
+
+
+Conditions
+if, else if, else
+=== == !== != > < >= <= 
+&& ||
+
+
+For Loop
+
+Array
+pop push shift unshift .length slice splice 
+
+Strings Methods
+Number Methods
+
+DOM 
+
+Date and Time
+*/
+
+
+
+
+
+
+
+
+// var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// var today = new Date();
+// var day = today.getDay();
+// console.log(today);
+
+// console.log(days[day]);
+
+
+// var month = today.getMonth();
+// console.log(month);
+
+
+// console.log(today.getDate());
+
+
+// console.log(today.getFullYear());
+
+
+
+// console.log(today.getHours());
+
+
+// console.log(today.getMinutes());
+
+
+// console.log(today.getSeconds());
+
+
+// console.log(today.getMilliseconds());
+
+// console.log(today.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var birthDate = new Date("October 4, 1991");
+// console.log(days[birthDate.getDay()]);
+
+// var birthMiliseconds = birthDate.getTime();
+
+// var birthDateWithMiliseconds= new Date(birthMiliseconds);
+
+// console.log(birthDate);
+// console.log(birthDateWithMiliseconds);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var marriageDate = new Date('March 23, 2022');
+// var today = new Date();
+// // console.log(marriageDate);
+// // console.log(today);
+
+// marriageDate = marriageDate.getTime();
+// today = today.getTime();
+
+// var diff = marriageDate - today;
+// var remainDays = diff / (1000 * 60 * 60 * 24);
+
+// console.log(Math.floor(remainDays));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var birthDate = new Date('October 4, 2020');
+var today = new Date();
+
+birthDate = birthDate.getTime();
+today = today.getTime();
+
+var diff = birthDate - today;
+
+var diff = today - birthDate
+var remainDays = diff / (1000 * 60 * 60 * 24);
+
+console.log(Math.floor(remainDays));
+
