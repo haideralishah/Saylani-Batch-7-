0b168f97202e16694b6e13606404399baa412820
haideralishah <headeralishah@gmail.com>
2021-05-17 10:58:43 +0500

    class code switch, while loop and do while loop

---
+++ JS/Chapter 40 to 44/app.js
@@ -0,0 +1,58 @@
+// var numOfDay = new Date().getDay();
+
+// var daysName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// var day = daysName[numOfDay];
+
+// switch (day) {
+//     case 'Mon':
+//         console.log('class hogi');
+//         break;
+//     case 'Sun':
+//         console.log('class ni hogi');
+//         break;
+//     default:
+//         console.log('let us confirm from admin');
+//         break;
+// }
+
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+
+
+// var randomNum = Math.ceil(Math.random() * 5);
+
+// while (randomNum !== 5) {
+//     console.log(randomNum);
+//     randomNum = Math.ceil(Math.random() * 5);
+// }
+
+
+
+var randomNum = 5;
+do {
+    console.log(randomNum);    
+} while (randomNum !== 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
