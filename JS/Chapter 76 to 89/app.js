743eae47036070bf69c66fd253f6556e4b1b87f8
haideralishah <headeralishah@gmail.com>
2021-06-21 10:44:02 +0500

    try catch navigator window location

---
+++ JS/Chapter 76 to 89/app.js
@@ -0,0 +1,117 @@
+// console.log(1);
+
+// try {
+//     Console.log(2);
+// } catch (e) {
+//     console.error(e);
+// }
+
+// console.log(3);
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(1);
+
+// try {
+
+//     throw "Welcome to Haider Ali"
+
+// } catch (e) {
+//     console.error(e);
+// }
+
+// console.log(3);
+
+
+
+
+
+
+
+// var passwordEl = document.getElementById('password-element');
+// var errorEl = document.getElementById('error-display');
+// function register() {
+//     var password = passwordEl.value;
+//     try {
+//         if (password.length < 8) {
+//             throw "Password length should be atleast 8 chars."
+//         }
+//         else if (password.indexOf('@') < 0) {
+//             throw "Password must contain @ sign."
+//         }
+//     } catch (e) {
+//         errorEl.innerHTML = e;
+//         hideError();
+//     }
+// }
+
+// function hideError() {
+//     setTimeout(function () {
+//         errorEl.innerHTML = "";
+//     }, 3000);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+http://www.mybeautifulsite.com/products/page33.html#humidifier
+
+
+href // http://www.mybeautifulsite.com/products/page33.html#humidifier
+protocol
+host name // .hostname  // www.mybeautifulsite.com
+path name  // .pathname // products/page33.html
+hash // hash // humidifier
+
+
+
+
+*/
+
+// var pageHref = window.location.href;
+// var pageHostName = window.location.hostname;
+// var pagePath = window.location.pathname;
+// var hash = window.location.hash;
+
+// console.log(pageHref);
+// console.log(pageHostName);
+// console.log(pagePath);
+// console.log(hash);
+
+
+// window.location.href = 'https://www.google.com';
+
+// window.location.assign("http://www.google.com");
+
+
+
+
+
+// setTimeout(function () {
+//     window.location.reload();
+// }, 3000)
+
+navigator.geolocation.getCurrentPosition(function (geoLoc) {
+    console.log(geoLoc);
+});
