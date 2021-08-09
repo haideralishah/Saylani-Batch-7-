45b7345497b68fa9923a90f068f078aaa93ee7cb
haideralishah <headeralishah@gmail.com>
2021-08-09 10:46:21 +0500

    signup signin completed findwork and my job UI created

--- hackathon preparation/supaari/app.js
+++ hackathon preparation/supaari/app.js
@@ -1,22 +1,38 @@
+let storage = firebase.storage();
+let auth = firebase.auth();
+let db = firebase.firestore();
+
+
+
 let userNameEl = document.getElementById('user-name');
 let emailEl = document.getElementById('email');
 let passwordEl = document.getElementById('psw');
 let passwordRepeatEl = document.getElementById('psw-repeat');
 let userRoleEl = document.getElementsByName('user-role');
 let userImageEl = document.getElementById('upload-image');
+let avatarCustomEl = document.getElementById('avatar-custom');
 
-function regiserUser() {
-    console.log('hello world', userRoleEl);
+function imageSelected() {
+    let image = userImageEl.files[0];
+    avatarCustomEl.src = `./images/${image.name}`;
+}
+
+
+async function regiserUser() {
+
+    let userCreated = await auth.createUserWithEmailAndPassword(emailEl.value, passwordEl.value);
+    let UID = userCreated.user.uid;
+    let imageURL = await uploadImageToStorage(UID);
 
     let user = {
         userName: userNameEl.value,
         email: emailEl.value,
-        password: passwordEl.value,
-        passwordRepeat: passwordRepeatEl.value,
-        userImage: userImageEl.files[0],
-        userRole: giveCheckedRadio()
+        userImage: imageURL,
+        userRole: giveCheckedRadio(),
+        uid: UID
     }
-    console.log(user)
+
+    await db.collection('users').doc(UID).set(user);
 
 }
 
@@ -29,3 +45,45 @@ function giveCheckedRadio() {
     }
     return checkedProp;
 }
+
+function uploadImageToStorage(UID) {
+    return new Promise(async (resolve, reject) => {
+        let image = userImageEl.files[0];
+        let storageRef = storage.ref();
+        let imageRef = storageRef.child(`avatar/${UID}/${image.name}`);
+        await imageRef.put(image);
+        let url = await imageRef.getDownloadURL();
+        resolve(url);
+    })
+}
+
+auth.onAuthStateChanged((user) => {
+    let pageLocArr = window.location.href.split('/');
+    let pageName = pageLocArr[pageLocArr.length - 1];
+    let authenticatedPages = ['home.html', 'findwork.html', 'myjob.html'];
+
+    if (user && authenticatedPages.indexOf(pageName) === -1) {
+        window.location = './findwork.html';
+    }
+    else if (!user && pageName === 'home.html') {
+        window.location = './index.html';
+    }
+});
+
+async function signout() {
+    await auth.signOut();
+}
+
+
+
+async function signinUser() {
+    await auth.signInWithEmailAndPassword(emailEl.value, passwordEl.value);
+}
+
+
+
+
+
+
+
+
