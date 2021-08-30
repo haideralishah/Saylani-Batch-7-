0dec601c959440f73ad7cc8698637fbfe88cf52e
haideralishah <headeralishah@gmail.com>
2021-08-30 11:03:17 +0500

    food menu app

--- hackathon preparation/Food Delivery/app.js
+++ hackathon preparation/Food Delivery/app.js
@@ -101,19 +101,36 @@ function uploadImageToStorage(UID) {
     })
 }
 
-auth.onAuthStateChanged((user) => {
+auth.onAuthStateChanged(async (user) => {
     let pageLocArr = window.location.href.split('/');
     let pageName = pageLocArr[pageLocArr.length - 1];
     let authenticatedPages = ['home.html', 'restaurant-home.html', 'myjob.html'];
+    let customerPages = ['customer-home.html', 'food-menus.html'];
+    let restaurantPages = ['restaurant-home.html', 'myjob.html'];
 
+    let unauthPages = ['index.html', 'signin.html'];
+    console.log(user, '*********');
 
+    // if (user && authenticatedPages.indexOf(pageName) === -1) {
+    //     // window.location = './restaurant-home.html';
+    // }
+    // else 
     if (user && authenticatedPages.indexOf(pageName) === -1) {
-        window.location = './restaurant-home.html';
+        let authenticatedUser = await db.collection('users').doc(user.uid).get();
+        authenticatedUser = authenticatedUser.data();
+        let authenticatedUserString = JSON.stringify(authenticatedUser);
+        localStorage.setItem('currentUser', authenticatedUserString);
+
+        if (authenticatedUser.userRole === 'customer' && customerPages.indexOf(pageName) === -1) {
+            window.location = './customer-home.html';
+        }
+        else if (authenticatedUser.userRole !== 'customer' && restaurantPages.indexOf(pageName) === -1) {
+            window.location = './restaurant-home.html';
+        }
     }
-    else if (!user && pageName === 'home.html') {
+    else if (!user && unauthPages.indexOf(pageName) === -1) {
         window.location = './index.html';
     }
-
 
 });
 
@@ -153,6 +170,22 @@ function setDishProp(propName, el) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Restaurant Work*/
+
 async function addDish() {
     let imageURL = await uploadImageToStorage(auth.currentUser.uid);
 
@@ -169,4 +202,119 @@ async function addDish() {
 
 }
 
+/*Restaurant Work*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Customer Work*/
+
+let restaurantDiv = document.getElementById('restaurants-list');
+async function fetchRestaurants() {
+    let allRestaurants = await db.collection('users').where('userRole', "==", 'restaurant').get();
+    allRestaurants.forEach((doc) => {
+        let restaurant = doc.data();
+        restaurant.id = doc.id;
+        let element = `<div id="${restaurant.id}" class="container container-img" onclick="selectRestaurant(this)">
+                            <img src="${restaurant.userImage}" alt="Restaurant Cover" class="image">
+                            <div class="overlay">${restaurant.userName}</div>
+                        </div>`;
+        restaurantDiv.innerHTML += element;
+
+    });
+}
+
+
+
+function selectRestaurant(el) {
+    console.log(el.id);
+    localStorage.setItem('selectedRestaurant', el.id);
+    window.location = './food-menus.html';
+}
+
+
+let foodMenusDiv = document.getElementById('food-menus');
+async function fetchSelectedRestaurant() {
+    let restId = localStorage.getItem('selectedRestaurant');
+    let foodMenus = await db.collection('foodmenus').where('uid', '==', restId).get();
+    foodMenus.forEach((doc) => {
+        let menus = doc.data();
+        menus.id = doc.id;
+        let el = `<div id="${menus.id}" class="container container-img" onclick="selectMenus(this)">
+                        <img src="${menus.dishImage}" alt="Restaurant Cover" class="image">
+                        <div class="overlay">${menus.itemName} Price:${menus.price}</div>
+                    </div>`;
+        foodMenusDiv.innerHTML += el;
+
+    })
+}
+
+
+
+
+
+function selectMenus(el) {
+    let cart = localStorage.getItem('cart');
+    let foodId = el.id;
+
+    if (!cart) cart = [];
+    else cart = JSON.parse(cart);
+    let existingIndex;
+    for (let i = 0; i < cart.length; i++) {
+        if (foodId === cart[i].foodId) {
+            existingIndex = i;
+            break;
+        }
+    }
+    console.log(cart, foodId, existingIndex)
+
+
+    if (existingIndex === undefined) {
+        let price = el.children[1].childNodes[0].nodeValue.split('Price:')[1]
+        let item = {
+            foodId: el.id,
+            quantity: 1,
+            price,
+            totalPrice: this.quantity * price
+        }
+        cart.push(item);
+    }
+    else {
+        let selectedItem = cart[existingIndex];
+        selectedItem.quantity++;
+        selectedItem.totalPrice = selectedItem.price * selectedItem.quantity;
+        console.log(selectedItem);
+    }
+
+
+
+
+
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+}
+
+
+
+
+
+
+
+
+
+/*Customer Work*/
 
