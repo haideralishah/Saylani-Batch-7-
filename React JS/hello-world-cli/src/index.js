b7e072f054578def7686a0bfad0ffb089a9ec52f
haideralishah <headeralishah@gmail.com>
2021-09-01 11:03:46 +0500

    React Hello World

---
+++ React JS/hello-world-cli/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+let h1 = React.createElement('h1', null, 'hello world cli');
+
+ReactDOM.render(h1, document.getElementById('root'));
